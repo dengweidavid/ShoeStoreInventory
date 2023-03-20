3cad185ad07093ba464fc11a82e83b64bb4ad67f
dengweidavid <wei.deng@machineswithvision.com>
2023-03-20 22:05:07 +0000

    Finish first three screens

---
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
